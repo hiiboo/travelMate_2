2540bd06ddfa68d7f78c93a396093397b6f994e4
Yugo <satoyugo1022@gmail.com>
2023-08-23 12:17:57 +0900

    added route definition

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Api\ArticleController;
+use App\Http\Controllers\Api\GenreController;
+use App\Http\Controllers\Api\LanguageController;
+use App\Http\Controllers\Api\ArticleTranslationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +22,26 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::apiResource(
+    'articles',
+    ArticleController::class
+)->only(['index', 'show']);
+
+Route::apiResource(
+    'articles.translations',
+    ArticleTranslationController::class
+)->only(['index', 'show']);
+
+Route::apiResource(
+    'genres',
+    GenreController::class
+)->only(['index', 'show']);
+
+Route::apiResource(
+    'languages',
+    LanguageController::class
+)->only(['index', 'show']);
+
+
+
