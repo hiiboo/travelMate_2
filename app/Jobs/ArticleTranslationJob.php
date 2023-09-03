cb8ce14c24ab34bc922637da6841abeb3030c355
Yugo <satoyugo1022@gmail.com>
2023-09-03 21:26:58 +0900

    add article translation func

--- app/Jobs/ArticleTranslationJob.php
+++ app/Jobs/ArticleTranslationJob.php
@@ -31,80 +31,102 @@ class ArticleTranslationJob implements ShouldQueue
     /**
      * Execute the job.
      */
+    // public function handle(): void
+    // {
+    //     Log::info('Start of handle method');
+    //     // $languages = Language::all();
+    //     $client = new Client();
+
+    // foreach ($languages as $language) {
+    //     Log::info('Start of foreach loop');
+
     public function handle(): void
     {
-        $languages = Language::all();
         $client = new Client();
 
-        foreach ($languages as $language) {
-            try {
-                $response = $client->post('https://api.openai.com/v1/chat/completions', [
-                    'headers' => [
-                        'Content-Type' => 'application/json',
-                        'Authorization' => 'Bearer ' . config('services.openai.api_key'),
-                    ],
-                    'json' => [
-                        'model' => "gpt-3.5-turbo",
-                        'messages' => [
-                            [
-                                'role' => 'system',
-                                'content' => "You are a helpful assistant that translates Japanese to {$language->name}.",
-                            ],
-                            [
-                                'role' => 'user',
-                                'content' => $this->article->content,
-                            ],
+        $this->translateAndSave($client, 'English');
+        $this->translateAndSave($client, 'French');
+        $this->translateAndSave($client, 'Spanish');
+    }
+
+    private function translateAndSave($client, $languageName)
+    {
+        Log::info('Start of handle method');
+        try {
+            $response = $client->post('https://api.openai.com/v1/chat/completions', [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Authorization' => 'Bearer ' . config('services.openai.api_key'),
+                ],
+                'json' => [
+                    'model' => "gpt-3.5-turbo",
+                    'messages' => [
+                        [
+                            'role' => 'system',
+                            'content' => "You are a helpful assistant that translates Japanese to {$languageName}.",
+                        ],
+                        [
+                            'role' => 'user',
+                            'content' => $this->article->content,
                         ],
                     ],
-                ]);
+                ],
+            ]);
+            Log::info('After sending request');
+            $responseContent = $response->getBody()->getContents();
+            Log::info($responseContent);
+            $response_json = json_decode($response->getBody(), true);
 
-                $response_json = json_decode($response->getBody(), true);
+            if (isset($response_json['error'])) {
+                Log::error('OpenAI API error: ' . $response_json['error']['message']);
+                return;
+            }
 
-                if (isset($response_json['error'])) {
-                    Log::error('OpenAI API error: ' . $response_json['error']['message']);
-                    continue;
-                }
-
-                $translated_content = $response_json['choices'][0]['message']['content'];
-
-                
-                $response = $client->post('https://api.openai.com/v1/chat/completions', [
-                    'headers' => [
-                        'Content-Type' => 'application/json',
-                        'Authorization' => 'Bearer ' . config('services.openai.api_key'),
-                    ],
-                    'json' => [
-                        'model' => "gpt-3.5-turbo",
-                        'messages' => [
-                            [
-                                'role' => 'system',
-                                'content' => "You are a helpful assistant that translates Japanese to {$language->name}.",
-                            ],
-                            [
-                                'role' => 'user',
-                                'content' => $this->article->title,
-                            ],
+            $translated_content = $response_json['choices'][0]['message']['content'];
+            Log::info('After getting translated content');
+            sleep(4); 
+            Log::info('Before sending request');
+            $response = $client->post('https://api.openai.com/v1/chat/completions', [
+                'headers' => [
+                    'Content-Type' => 'application/json',
+                    'Authorization' => 'Bearer ' . config('services.openai.api_key'),
+                ],
+                'json' => [
+                    'model' => "gpt-3.5-turbo",
+                    'messages' => [
+                        [
+                            'role' => 'system',
+                            'content' => "You are a helpful assistant that translates Japanese to {$languageName}.",
+                        ],
+                        [
+                            'role' => 'user',
+                            'content' => $this->article->title,
                         ],
                     ],
-                ]);
+                ],
+            ]);
+            Log::info('After sending request');
+            $responseContent = $response->getBody()->getContents();
+            Log::info($responseContent);
+            $response_json = json_decode($response->getBody(), true);
 
-                $response_json = json_decode($response->getBody(), true);
-
-                if (isset($response_json['error'])) {
-                    Log::error('OpenAI API error: ' . $response_json['error']['message']);
-                    continue;
-                }
-
-                $translated_title = $response_json['choices'][0]['message']['content'];
-
-                ArticleTranslation::updateOrCreate(
-                    ['article_id' => $this->article->id, 'language_id' => $language->id],
-                    ['title' => $translated_title, 'content' => $translated_content]
-                );
-            } catch (\Exception $e) {
-
-                Log::error('Exception encountered while translating article: ' . $e->getMessage());
+            if (isset($response_json['error'])) {
+                Log::error('OpenAI API error: ' . $response_json['error']['message']);
+                return;
             }
-            }
+
+            $translated_title = $response_json['choices'][0]['message']['content'];
+            Log::info('After getting translated content');
+            $language = Language::where('name', $languageName)->first();
+
+            ArticleTranslation::updateOrCreate(
+                ['article_id' => $this->article->id, 'language_id' => $language->id],
+                ['title' => $translated_title, 'content' => $translated_content]
+            );
+        } catch (\Exception $e) {
+            Log::error('Exception encountered while translating article: ' . $e->getMessage());
         }
+        sleep(4); 
+    }
+
 }
